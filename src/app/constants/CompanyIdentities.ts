fdcf5d3ca8780f666320d39a63a86dc1d5ec1820
Ignacio <ignacio@ripiocredit.network>
2018-07-04 18:37:52 -0300

    Raleway Font - Google Fonts @font-face

--- src/app/constants/CompanyIdentities.ts
+++ src/app/constants/CompanyIdentities.ts
@@ -5,7 +5,7 @@ export const companyIdentities = {
     [Agent.RipioCreator]: new CompanyIdentity(
             'Ripio',
             'Ripio is one of the leading Bitcoin wallets in Latin America. ... Ripio is offering its services across several Latin American countries.',
-            'https://ripio-cms-us.s3.amazonaws.com/filer_public/80/d7/80d76109-a560-446c-9385-d6d911168dbe/logo-ripio-white.svg',
+            '../../assets/logo-ripio-white.svg',
             new Date()
         )
 };
